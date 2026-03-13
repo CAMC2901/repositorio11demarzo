e827d95a74f87b470dacd3e91189a34c66eed0bc
camilo <cacb2908@gmail.com>
2026-03-12 22:54:36 -0500

    cuarto commit

--- 14cine.py
+++ 14cine.py
@@ -13,19 +13,19 @@ while acum_per<=capacidad and boletos!="salir":
            print("no hay suficientes cupos")
         else:
            acum_per+=personas
-        for i in range(personas):
-            edad=int(input(" ponga su edad: "))
-            if edad<18:
+           for i in range(personas):
+             edad=int(input(" ponga su edad: "))
+             if edad<18:
                 acum_niños+=1
                 print("niño")
-            elif edad>=18 and edad<60:
+             elif edad>=18 and edad<60:
                 acum_adulto+=1
                 print("adulto")
-            elif edad>=60:
+             elif edad>=60:
                 acum_adultomayor+=1
                 print("adulto mayor")
-            elif boletos=="salir":
-             print("saliendo")
+             elif boletos=="salir":
+                print("saliendo")
 print(f"ingresaron {acum_per} personas.")
 print(f"ingresaron {acum_niños} niños, {acum_adulto} adultos, {acum_adultomayor} adultos mayores.")
 if acum_per == capacidad:
--- 3cafeteria.py
+++ 3cafeteria.py
@@ -3,15 +3,15 @@ te=3500
 jugo=5000
 pedido=int(input("1. cafe, 2. te, 3. jugo"))
 if pedido == 1:
-    cant_cafe=int(input("cuantos cafe quiere: "))
-    total_cafe=cant_cafe*cafe
+    cant=int(input("cuantos cafe quiere: "))
+    total_cafe=cant*cafe
     print("total de su compra es: ", total_cafe)
 elif pedido == 2:
-    cant_te=int(input("cuantos te quiere: "))
-    total_te=cant_te*te
+    cant=int(input("cuantos te quiere: "))
+    total_te=cant*te
     print("el total de su compra es: ", total_te)
 else:
-    cant_jugo=int(input("cuantos jugos quiere: "))
-    total_jugo=cant_jugo*jugo
+    cant=int(input("cuantos jugos quiere: "))
+    total_jugo=cant*jugo
     print("el total de su compra es: ", total_jugo)
     
